9f9fc979f88f409b425c9ee13abec0e436d51ee3
elsesir <sirandelsa@163.com>
2017-08-24 23:36:05 +0800

    修改练习显示

---
+++ src/js/blog-homepage.js
@@ -0,0 +1,14 @@
+/**
+ * Created by bigWhite on 17/8/24.
+ */
+$(document).ready(function() {
+    $('.a_lianxi').hover( function(){
+        $('.lianxi').show();
+    });
+    $('.lianxi').hover(function(){}, function() {
+        $('.lianxi').toggle();
+    });
+    $('.a_lianxi').click( function(){
+        $('.lianxi').show();
+    });
+});
