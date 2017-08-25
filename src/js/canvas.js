7e35181254d39b8da7ebc5c3653ec7da8c4a3176
elsesir <sirandelsa@163.com>
2017-08-25 11:14:36 +0800

    测试手机端页面

---
+++ src/js/canvas.js
@@ -0,0 +1,11 @@
+/**
+ * Created by bigWhite on 17/8/24.
+ */
+window.onload = function() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(10, 10, 100, 100);
+    console.log(111);
+};
